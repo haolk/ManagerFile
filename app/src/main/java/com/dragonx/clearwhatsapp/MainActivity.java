87ba8e18df4bfcde5a443aab6660bd5bcc1c006d
hao.lek <hao.lek@qup.world.com>
2018-11-27 17:46:27 +0700

    get data

--- app/src/main/java/com/dragonx/clearwhatsapp/MainActivity.java
+++ app/src/main/java/com/dragonx/clearwhatsapp/MainActivity.java
@@ -1,43 +1,25 @@
 package com.dragonx.clearwhatsapp;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
-import java.security.Permission;
-import java.util.Arrays;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.Scheduler;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.List;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends AppCompatActivity {
 
     @ViewById(R.id.toolbar)
     Toolbar toolbar;
@@ -45,35 +27,48 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private static final int PERMISSION_CODE = 1001;
     private static final int LOADER_PICTURE = 1002;
     private ViewModel mViewModel;
+    private Store mStore;
 
     @AfterViews
     void afterViews() {
-        mViewModel = new ViewModel();
+//        mViewModel = new ViewModel();
         setSupportActionBar(toolbar);
         checkPermission();
+        mStore = new Store(this);
+        initData();
     }
 
     private void initData() {
 //        MainActivity.this.getSupportLoaderManager().initLoader(LOADER_PICTURE, null, this);
-        MainActivity.this.getSupportLoaderManager().initLoader(LOADER_PICTURE, null, this);
+//        MainActivity.this.getSupportLoaderManager().initLoader(LOADER_PICTURE, null, this);
+        List<File> imagesPrivate = mStore.getFiles(Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsApp/Media/WhatsApp Images/Private",
+                new String[]{"png", "png", "jpg"});
+        List<File> imagesSent = mStore.getFiles(Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsApp/Media/WhatsApp Images/Sent",
+                new String[]{"png", "png", "jpg"});
+
+        List<File> videoPrivate = mStore.getFiles(Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsApp Video/Private",
+                new String[]{"3gp", "mp4", "jpg"});
+        List<File> videoSent = mStore.getFiles(Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsApp Video/Sent",
+                new String[]{"3gp", "mp4", "jpg"});
+
+        List<File> audioPrivate = mStore.getFiles(Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsApp Audio/Private",
+                new String[]{"mp3", "flac", "m4a"});
+        List<File> audioSent = mStore.getFiles(Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsApp Audio/Sent",
+                new String[]{"mp3", "flac", "m4a"});
+
+
+        List<File> databases = mStore.getFiles(Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsApp/Databases");
+        Log.e("HAOHAO", "/WhatsApp/Media/WhatsApp Images/Private: " + imagesPrivate.size());
     }
 
     private void checkPermission() {
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
-            } else {
-                initData();
-            }
-        } else {
-            initData();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
         }
-
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             initData();
         } else {
@@ -81,58 +76,69 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     }
 
-    @NonNull
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
-        return new CursorLoader(this, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Constant.PICTURE_DATA,
-                Constant.PICTURE_WHERE, Constant.PICTURE_TYPE, Constant.PICTURE_DATA[2] + " DESC");
-    }
+//    @NonNull
+//    @Override
+//    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+//        return new CursorLoader(this, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Constant.PICTURE_DATA,
+//                Constant.PICTURE_WHERE, Constant.PICTURE_TYPE, Constant.PICTURE_DATA[2] + " DESC");
+//    }
+//
+//    @SuppressLint("CheckResult")
+//    @Override
+//    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+//        ViewModel.getAllPicture(cursor)
+//                .observeOn(Schedulers.io())
+//                .subscribe(handleGetPictureSuccess);
+//        removeLoader();
+//    }
+//
+//    Observer<PictureFolder> handleGetPictureSuccess = new Observer<PictureFolder>() {
+//
+//        @Override
+//        public void onSubscribe(Disposable d) {
+//        }
+//
+//        @Override
+//        public void onNext(PictureFolder pictureFolder) {
+//            // TODO
+//            Log.e("HAOHAO", "haha " + pictureFolder.getSize());
+//        }
+//
+//        @Override
+//        public void onError(Throwable e) {
+//
+//        }
+//
+//        @Override
+//        public void onComplete() {
+//
+//        }
+//    };
+//
+//    @Override
+//    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+//    }
+//
+//    private void removeLoader() {
+//        if (getSupportLoaderManager().hasRunningLoaders()) {
+//            getSupportLoaderManager().destroyLoader(LOADER_PICTURE);
+//        }
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        removeLoader();
+//    }
 
-    @SuppressLint("CheckResult")
-    @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
-        ViewModel.getAllPicture(cursor)
-                .observeOn(Schedulers.io())
-                .subscribe(handleGetPictureSuccess);
-        removeLoader();
-    }
-
-    Observer<PictureFolder> handleGetPictureSuccess = new Observer<PictureFolder>() {
-
-        @Override
-        public void onSubscribe(Disposable d) {
-        }
-
-        @Override
-        public void onNext(PictureFolder pictureFolder) {
-            // TODO
-            Log.e("HAOHAO", "haha " + pictureFolder.getSize());
-        }
-
-        @Override
-        public void onError(Throwable e) {
-
-        }
-
-        @Override
-        public void onComplete() {
-
-        }
-    };
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-    }
-
-    private void removeLoader() {
-        if (getSupportLoaderManager().hasRunningLoaders()) {
-            getSupportLoaderManager().destroyLoader(LOADER_PICTURE);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        removeLoader();
+    /**
+     * Func format size
+     */
+    private String formatSize(Long size) {
+        if (size <= 0)
+            return "0";
+        String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024.0));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024.0, digitGroups)) + " " + units[digitGroups];
     }
 }
---
+++ app/src/main/java/com/dragonx/clearwhatsapp/Store.java
@@ -0,0 +1,59 @@
+package com.dragonx.clearwhatsapp;
+
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author HaoLeK
+ * Created on 27/11/2018
+ */
+public class Store {
+
+    private Context context;
+
+    public Store(Context context) {
+        this.context = context;
+    }
+
+    public String getExternalStoreDirectory() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath();
+    }
+
+    public List<File> getFiles(String dir) {
+        return getFiles(dir, null);
+    }
+
+    public List<File> getFiles(String dir, final String[] matchRegex) {
+        File file = new File(dir);
+        File[] files;
+        if (matchRegex != null) {
+            FilenameFilter filter = new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    for (String regex : matchRegex) {
+                        if (name.endsWith("." + regex)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            };
+            files = file.listFiles(filter);
+        } else {
+            files = file.listFiles();
+        }
+        return files != null ? Arrays.asList(files) : null;
+    }
+
+    public void deleteFiles(String[] paths) {
+        for (String path : paths) {
+            File file = new File(path);
+            file.delete();
+        }
+    }
+}
